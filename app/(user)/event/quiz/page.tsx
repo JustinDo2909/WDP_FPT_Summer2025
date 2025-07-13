97e2886ac0c4a5933f7b96366824948fb0e782c1
MSI\KHANG <usukuruyuhk080303@gmail.com>
2025-07-13 20:42:15 +0700

    fix error run build

--- app/(user)/event/quiz/page.tsx
+++ app/(user)/event/quiz/page.tsx
@@ -1,7 +1,7 @@
 import Quiz_game from "@/components/Event/Quiz/quiz_game";
 import { Container } from "@/lib/by/Div";
 
-export default function Game() {
+export default function Page() {
   return (
     <Container>
       <Quiz_game />
